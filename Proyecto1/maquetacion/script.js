928ad49dd8d005fcda320fc097514ed945fdbb6b
ShBluze <shazeBluze@proton.me>
2025-05-19 21:31:38 +0200

    menu desplegable

---
+++ Proyecto1/maquetacion/script.js
@@ -0,0 +1,7 @@
+// const lenguajes = document.getElementById("lenguajes");
+
+
+// lenguajes.addEventListener("mouseenter", ()=>{
+//     lenguajes.style.display = "block";
+//     lenguajes.style.background = "red";
+// })
